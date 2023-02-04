7c8b712ba57e47699c3c26f07b60e1f64b2b84e6
Daniel Boiko <47049059+GorinichSerpant@users.noreply.github.com>
2023-02-04 12:13:15 -0700

    fixed player movement

--- Character/Player.gd
+++ Character/Player.gd
@@ -1,8 +1,11 @@
 extends KinematicBody2D
 
 
-const GRAVITY = 200.0
+const GRAVITY = 100.0
 const WALK_SPEED = 200
+const JUMP_SPEED = 150
+const MAX_JUMPS = 1
+var JUMPS_AVALIABLE = MAX_JUMPS
 
 var velocity = Vector2()
 
@@ -15,9 +18,12 @@ func _physics_process(delta):
 		velocity.x =  WALK_SPEED
 	else:
 		velocity.x = 0
-
-	# We don't need to multiply velocity by delta because "move_and_slide" already takes delta time into account.
-
-	# The second parameter of "move_and_slide" is the normal pointing up.
-	# In the case of a 2D platformer, in Godot, upward is negative y, which translates to -1 as a normal.
-	move_and_slide(velocity, Vector2(0, -1))
+	
+	if Input.is_action_just_pressed("jump") and JUMPS_AVALIABLE > 0:
+		velocity.y = -JUMP_SPEED
+		JUMPS_AVALIABLE -= 1
+	
+	move_and_slide(velocity, Vector2(0, -1))\
+	
+	if is_on_floor():
+		JUMPS_AVALIABLE = MAX_JUMPS
